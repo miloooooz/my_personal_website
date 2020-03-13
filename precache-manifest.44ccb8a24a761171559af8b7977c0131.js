15e6f686831273239002cd49ba630efb9f3e747b
miloooooz <zhouruijun0510@hotmail.com>
2020-03-13 23:23:09 +0800

    Adding files

---
+++ precache-manifest.44ccb8a24a761171559af8b7977c0131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bf051adc9cc5de32117",
+    "url": "/static/css/main.9c800889.chunk.css"
+  },
+  {
+    "revision": "3bf051adc9cc5de32117",
+    "url": "/static/js/main.3bf051ad.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0d328587d56e3fcbcb42ac92b230e13",
+    "url": "/index.html"
+  }
+];
